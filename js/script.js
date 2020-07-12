6b338831e331215f467006e038c028a2d5969d50
jorgejunior618 <jorgejunior618@gmail.com>
2020-07-11 23:01:30 -0300

    Ending of development stage

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-import Animations from './modules/animations.js';
 import Slider from './modules/slider.js';
 import ChangeTheme from './modules/changeTheme.js';
 
-Animations.initialPresentation();
 Slider.cardingSlide();
 ChangeTheme.themeChange();
 ChangeTheme.keyChangeTheme();
