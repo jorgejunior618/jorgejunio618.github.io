60ee8f15bd064438388d40180bdf1e5c225c611d
jorgejunior618 <jorgejunior618@gmail.com>
2023-06-08 18:37:24 -0300

    v2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-import Slider from './modules/slider.js';
-import ChangeTheme from './modules/changeTheme.js';
+const [eu, experiencia, projetos] = document.querySelectorAll("nav ul li");
+const [secteu, sectexperiencia, sectprojetos] = document.querySelectorAll("section");
 
-Slider.cardingSlide();
-ChangeTheme.themeChange();
-ChangeTheme.keyChangeTheme();
+const mainEl = document.querySelector('main');
+mainEl.addEventListener('scroll', () => {
+  const posAtual = mainEl.scrollTop;
+  const posicaoexperiencia = secteu.offsetHeight - 250;
+  const posicaoprojetos = posicaoexperiencia + sectexperiencia.offsetHeight;
+
+  console.log({posAtual, posicaoexperiencia, posicaoprojetos});
+  
+  if (posAtual <= posicaoexperiencia) {
+    eu.className = 'active';
+    experiencia.className = '';
+    // projetos.className = '';
+  } else/*  if (posAtual <= posicaoprojetos) */ {
+    eu.className = '';
+    experiencia.className = 'active';
+    // projetos.className = '';
+  }/*  else {
+    eu.className = '';
+    experiencia.className = '';
+    projetos.className = 'active';
+  } */
+})
